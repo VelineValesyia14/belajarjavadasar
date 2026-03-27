097f784ced86e7d874384009cdfe2aaa69d0ce40
Veline Valesyia <vvalesyia1404@gmail.com>
2026-03-27 20:13:03 +0700

    update class LoopingWhile

---
+++ src/main/java/com/mycompany/belajarjavadasar/LoopWhile.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.belajarjavadasar;
+
+import java.util.Scanner;
+
+public class LoopWhile {
+    public static void main(String[] args){
+        Scanner scan = new Scanner (System.in);
+        int angkaRahasia = 7;
+        int tebakan = 0;
+        
+        while (tebakan != angkaRahasia) {
+            System.out.println("tebak angka (1-10):");
+            tebakan = scan.nextInt();
+            
+            if (tebakan != angkaRahasia){
+                System.out.println("salah, coba lagi.");
+            }
+        }
+        System.out.println("benar! Angka rahasianya adalah "+ angkaRahasia);
+        scan.close();
+    }
+}
