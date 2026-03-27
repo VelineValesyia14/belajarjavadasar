28a6cf1071d8deb76a1fb9cb9a587dc45db1d7a3
Veline Valesyia <vvalesyia1404@gmail.com>
2026-03-27 20:08:26 +0700

    update class LoopingFor

--- src/main/java/com/mycompany/belajarjavadasar/LoopFor.java
+++ src/main/java/com/mycompany/belajarjavadasar/LoopFor.java
@@ -9,5 +9,10 @@ package com.mycompany.belajarjavadasar;
  * @author Hype
  */
 public class LoopFor {
-    
+    public static void main(String[] args) {
+        for (int i=1; i <=5; i++){
+            System.out.println("angka ke-"+i);
+        }
+        System.out.println("perulangan selesai.");
+    }
 }
