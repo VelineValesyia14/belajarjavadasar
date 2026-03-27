c8f69de466acecf1100af75e12a7b7b2a615f58c
Veline Valesyia <vvalesyia1404@gmail.com>
2026-03-27 21:18:53 +0700

    update class InputArray

--- src/main/java/com/mycompany/belajarjavadasar/InputArray.java
+++ src/main/java/com/mycompany/belajarjavadasar/InputArray.java
@@ -4,10 +4,31 @@
  */
 package com.mycompany.belajarjavadasar;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Hype
  */
 public class InputArray {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Berapa banyak data yang mau disimpan?");
+        int jumlah = scan.nextInt();
+        
+        int[] data = new int[jumlah];
+        for (int i=0; i<jumlah; i++){
+            System.out.print("masukkan data ke-" + (i + 1) + ":");
+            data[i] = scan.nextInt();
+        }
+        
+        System.out.println("\n -- Data yang Tersimpan ---");
+        for (int i=0; i < jumlah; i++){
+            System.out.println("Data[" +i+"]="+data[i]);
+        }
+        scan.close();
+                
+    }
+    
     
 }
